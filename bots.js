431fac5e4360985c167040f6a6c4d2651742d798
ElBachira <bachirabastian@gmail.com>
2025-08-13 17:02:09 -0600

    Create bots.js

---
+++ bots.js
@@ -0,0 +1,16 @@
+// Archivo: bots.js (ubicado en el repositorio elbachira.github.io)
+const BOTS_LIST = [
+    {
+        "nombre": "Acelin",
+        "url": "https://elbachira.github.io/Acelin/",
+        "imagen": "https://i.postimg.cc/jSfvCv6D/Acelin.png",
+        "genero": "masculino"
+    },
+    {
+        "nombre": "Stellan",
+        "url": "https://elbachira.github.io/Stellan/",
+        "imagen": "https://i.postimg.cc/RqxvH0BG/Stellan.png",
+        "genero": "masculino"
+    },
+    // ... Y así con todos tus otros 73 bots ...
+];
