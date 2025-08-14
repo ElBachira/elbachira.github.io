a69f203147bfc6dd29fc007aaf01cf3c855442f0
ElBachira <bachirabastian@gmail.com>
2025-08-13 19:38:32 -0600

    Update bots.js

--- bots.js
+++ bots.js
@@ -1,5 +1,7 @@
-// Forzando la actualización
+// Archivo bots.js completo y actualizado
+
 const BOTS_LIST = [
+    // --- BOTS MASCULINOS ---
     {
         "nombre": "Acelin",
         "url": "https://elbachira.github.io/Acelin/",
@@ -12,5 +14,28 @@ const BOTS_LIST = [
         "imagen": "https://i.postimg.cc/RqxvH0BG/Stellan.png",
         "genero": "masculino"
     },
-    // ... Y así con todos tus otros 73 bots ...
+    {
+        "nombre": "Bom-seok", // ¡No olvides añadir a Bom-seok también!
+        "url": "https://elbachira.github.io/Bom-seok/", // URL de su página
+        "imagen": "https://i.postimg.cc/W4N2Wq6b/stellan.png", // La URL de su imagen
+        "genero": "masculino"
+    },
+
+    // --- BOTS FEMENINOS ---
+    {
+        "nombre": "Aceline",
+        "url": "https://elbachira.github.io/Aceline/",
+        "imagen": "https://i.postimg.cc/L6kcDSFq/Aceline.png",
+        "genero": "femenino" // <-- La clave mágica
+    },
+    {
+        "nombre": "Estelle",
+        "url": "https://elbachira.github.io/Estelle/",
+        "imagen": "https://i.postimg.cc/j5T9PJwD/Estelle.png",
+        "genero": "femenino" // <-- La clave mágica
+    }
+
+    // --- Aquí seguirás añadiendo el resto de tus bots ---
+    // Simplemente copia un bloque, pégalo y cambia los datos.
+    // Recuerda poner una coma , entre cada bloque.
 ];
